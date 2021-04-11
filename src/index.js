ff93bf5c8c6df9850b8207a40f48f6178fd69841
pierlucapinoia <pierluca.pinoia@gmail.com>
2021-04-11 23:01:20 +0200

    Update

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+//Define paths for express config
+const viewsPath = path.join(__dirname, '../templates/hbs/views')
+const partialsPath = path.join(__dirname, '../templates/hbs/partials')
+
+//Setting hbs config
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    res.render('index')
+    // res.send('Hello world')
+})
+
+app.listen(3000)
